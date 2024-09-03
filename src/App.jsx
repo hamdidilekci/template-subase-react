6e7778009e595944ffa2351a8dea6e328a7ea168
Hamdi Dilekçi <dilekcihamdi@gmail.com>
2024-09-03 15:29:37 +0300

    get countries data from subase

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+    "https://mmrilehfdinkxcnuctwe.supabase.co",
+    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1tcmlsZWhmZGlua3hjbnVjdHdlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjUzNjIyNjUsImV4cCI6MjA0MDkzODI2NX0.IfaATjuwAfeZTaC8HDATGM5CVRJyBgfxpJrs98TwKg0"
+);
+
+console.log("supabase", supabase);
 
 function App() {
-  const [count, setCount] = useState(0)
+    const [countries, setCountries] = useState([]);
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    useEffect(() => {
+        getCountries();
+    }, []);
+
+    async function getCountries() {
+        try {
+            const { data, error } = await supabase.from("countries").select();
+
+            if (error) {
+                console.error("Error fetching countries: ", error.message);
+            } else if (data.length === 0) {
+                console.log("No countries found.");
+            } else {
+                console.log("Countries fetched successfully: ", data);
+                setCountries(data);
+            }
+        } catch (error) {
+            console.error("Unexpected error: ", error.message);
+        }
+    }
+
+    return (
+        <ul>
+            {countries.map((country) => (
+                <li key={country.name}>{country.name}</li>
+            ))}
+        </ul>
+    );
 }
 
-export default App
+export default App;
